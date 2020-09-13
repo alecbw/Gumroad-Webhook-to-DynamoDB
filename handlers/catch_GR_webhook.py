2bad745b1eeec0e345b509ece1dcf1ccdc0e4454
Alec <abarrett.wilsdon@gmail.com>
2020-09-13 00:09:27 -0700

    think i might have a solutiont o etl into GA

--- handlers/catch_GR_webhook.py
+++ handlers/catch_GR_webhook.py
@@ -6,6 +6,7 @@ from urllib.parse import parse_qs
 import logging
 
 import boto3
+import requests
 
 ############################################################################################
 
@@ -29,7 +30,7 @@ def lambda_handler(event, context):
     data_to_write = {
         "email": webhook_data.pop("email"),
         "timestamp": int(timestamp.timestamp()),
-        "value": int(webhook_data.pop("price")),
+        "value": int(webhook_data.pop("price")), # need to divide by 100 later fyi
         "offer_code": webhook_data.pop("offer_code", "No Code"),
         "country": webhook_data.pop("ip_country", "Unknown"),
         "refunded": 1 if webhook_data.pop("refunded") in ["true", "True", True] else 0,
@@ -40,10 +41,30 @@ def lambda_handler(event, context):
 
     success = write_dynamodb_item(data_to_write, "GRWebhookData")
 
+    track_google_analytics_event(data_to_write)
+
     return package_response(f"Success status was {success}", 200)
 
 
 ############################################################################################
+
+def track_google_analytics_event(data_to_write):
+    tracking_url = 'https://www.google-analytics.com/collect?v=1&t=event'
+    tracking_url += '&tid=' + 'UA-131042255-2'
+    tracking_url += "&ec=" + "product-" + ez_get(data_to_write, "data", "permalink") # event category
+    tracking_url += "&ea=" + "purchased" # event action
+    tracking_url += "&el=" + "purchased a product" # event label
+    tracking_url += "&ev=" + str(ez_get(data_to_write, "value")/100) # value
+    tracking_url += "&cid=" + ez_get(data_to_write, "_ga") # Anon Client ID (actually GA Session ID sent Cross-Domain)
+    tracking_url += '&aip=1'
+
+    # Not used in traditional event tracking
+    # tracking_url += "&cu=" + ez_get(data, "data", "currency") # currency
+
+    print(tracking_url)
+    resp = requests.post(tracking_url)
+    print(resp)
+    return
 
 
 # Note: this will BY DEFAULT overwrite items with the same primary key (upsert)
@@ -60,3 +81,16 @@ def write_dynamodb_item(dict_to_write, table, **kwargs):
 
     if not kwargs.get("disable_print"): logging.info(f"Successfully did a Dynamo Write to {table}")
     return True
+
+
+
+
+
+    # event_category = "product-" + ez_get(data, "data", "permalink")
+    # event_action = "purchased"
+    # event_label = "
+    #
+    # tracking_id = 'UA-131042255-2'
+	# clientid_str = str(datetime.now())
+	# tid='+tracking_id+'&cid='+clientid_str+'&ec='+event_category+'&ea='+event_action+'&el='+event_label+'&aip=1'
+    	# tracking_url = 'https://www.google-analytics.com/collect?v=1&t=event&tid='+tracking_id+'&cid='+clientid_str+'&ec='+event_category+'&ea='+event_action+'&el='+event_label+'&aip=1'
